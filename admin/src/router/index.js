4d95733555a4e8ad1c82b2732870a022b85f9dcf
zhuxin <624042309@qq.com>
2020-06-19 16:15:23 +0800

    文章

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -7,6 +7,8 @@ import ItemEdit from '../views/ItemEdit'
 import ItemList from '../views/ItemList'
 import HeroEdit from '../views/HeroEdit'
 import HeroList from '../views/HeroList'
+import ArticleEdit from '../views/ArticleEdit'
+import ArticleList from '../views/ArticleList'
 
 Vue.use(VueRouter)
 
@@ -27,6 +29,10 @@ const routes = [
       { path: '/heroes/create', component: HeroEdit },
       { path: '/heroes/edit/:id', component: HeroEdit, props: true },
       { path: '/heroes/list', component: HeroList },
+
+      { path: '/articles/create', component: ArticleEdit },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+      { path: '/articles/list', component: ArticleList },
     ]
   }
 ]
---
+++ admin/src/views/ArticleEdit.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="about">
+    <h1>{{id ? '编辑' : '新建'}}分类</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="分类">
+        <el-select v-model="model.categories" multiple>
+          <el-option v-for="item in categories" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="标题">
+            <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: {}
+    },
+    data() {
+      return {
+        model: {},
+        categories: []
+      }
+    },
+    methods: {
+      async save() {
+        let res
+        let message
+        if (this.id) {
+          await this.$http.put(`rest/articles/${this.id}`, this.model)
+          message = "更新成功"
+        } else {
+          await this.$http.post('rest/articles', this.model)
+          message = "保存成功"
+        }
+        console.log(res)
+        this.$router.push('/articles/list')
+        this.$message({
+          type: 'success',
+          message: message
+        })
+      },
+      async fetch() {
+        const res = await this.$http.get(`/rest/articles/${this.id}`)
+        this.model = res.data
+      },
+      async fetchCategories() {
+        const res = await this.$http.get(`/rest/categories`)
+        this.categories = res.data
+      }
+    },
+    created() {
+      this.fetchCategories()
+      this.id && this.fetch()
+    }
+  }
+</script>
