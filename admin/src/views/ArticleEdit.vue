d628b2f6978fc0fccdade91f8916af9b94bdf416
zhuxin <624042309@qq.com>
2021-06-10 16:49:37 +0800

    文章编辑

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>{{id ? '编辑' : '新建'}}分类</h1>
+    <h1>{{ id ? '编辑' : '新建' }}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="分类">
         <el-select v-model="model.categories" multiple>
@@ -8,7 +8,10 @@
         </el-select>
       </el-form-item>
       <el-form-item label="标题">
-            <el-input v-model="model.title"></el-input>
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item label="详情">
+        <vue-editor useCustomImageHandler @image-added="handleImageAdded" v-model="model.body"></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -19,46 +22,60 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      id: {}
-    },
-    data() {
-      return {
-        model: {},
-        categories: []
-      }
-    },
-    methods: {
-      async save() {
-        let res
-        let message
-        if (this.id) {
-          await this.$http.put(`rest/articles/${this.id}`, this.model)
-          message = "更新成功"
-        } else {
-          await this.$http.post('rest/articles', this.model)
-          message = "保存成功"
-        }
-        console.log(res)
-        this.$router.push('/articles/list')
-        this.$message({
-          type: 'success',
-          message: message
-        })
-      },
-      async fetch() {
-        const res = await this.$http.get(`/rest/articles/${this.id}`)
-        this.model = res.data
-      },
-      async fetchCategories() {
-        const res = await this.$http.get(`/rest/categories`)
-        this.categories = res.data
-      }
-    },
-    created() {
-      this.fetchCategories()
-      this.id && this.fetch()
+import {VueEditor} from 'vue2-editor'
+
+export default {
+  props: {
+    id: {}
+  },
+  components: {
+    VueEditor
+  },
+  data() {
+    return {
+      model: {},
+      categories: []
     }
+  },
+  methods: {
+    async save() {
+      let res
+      let message
+      if (this.id) {
+        await this.$http.put(`rest/articles/${this.id}`, this.model)
+        message = "更新成功"
+      } else {
+        await this.$http.post('rest/articles', this.model)
+        message = "保存成功"
+      }
+      console.log(res)
+      this.$router.push('/articles/list')
+      this.$message({
+        type: 'success',
+        message: message
+      })
+    },
+    async fetch() {
+      const res = await this.$http.get(`/rest/articles/${this.id}`)
+      this.model = res.data
+    },
+    async fetchCategories() {
+      const res = await this.$http.get(`/rest/categories`)
+      this.categories = res.data
+    },
+
+    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const res = await this.$http.post("upload", formData)
+      Editor.insertEmbed(cursorLocation, "image", res.data.url)
+      resetUploader()
+    }
+  },
+  created() {
+    this.fetchCategories()
+    this.id && this.fetch()
   }
+}
 </script>
