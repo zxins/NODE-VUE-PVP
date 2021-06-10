69c4ac73ec0d3a56327ba43e97eaa79bea648e57
zhuxin <624042309@qq.com>
2021-06-10 16:51:00 +0800

    管理员&优化其他内容

---
+++ admin/src/views/AdminUserEdit.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about">
+    <h1>{{id ? '编辑' : '新建'}}管理员</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="用户名">
+            <el-input v-model="model.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="model.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: {}
+    },
+    data() {
+      return {
+        model: {},
+        parents: []
+      }
+    },
+    methods: {
+      async save() {
+        let res
+        let message
+        if (this.id) {
+          await this.$http.put(`rest/categories/${this.id}`, this.model)
+          message = "更新成功"
+        } else {
+          await this.$http.post('rest/categories', this.model)
+          message = "保存成功"
+        }
+        console.log(res)
+        this.$router.push('/categories/list')
+        this.$message({
+          type: 'success',
+          message: message
+        })
+      },
+      async fetch() {
+        const res = await this.$http.get(`/rest/categories/${this.id}`)
+        this.model = res.data
+      },
+      async fetchParents() {
+        const res = await this.$http.get(`/rest/categories`)
+        this.parents = res.data
+      }
+    },
+    created() {
+      this.fetchParents()
+      this.id && this.fetch()
+    }
+  }
+</script>
